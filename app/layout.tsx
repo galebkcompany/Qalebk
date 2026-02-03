20748284c2e4f580bf3eecf5565a3299ab039779
muhanadibrahum90 <muhanadibrahum90@gmail.com>
2026-02-03 23:09:51 +0300

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -115,9 +115,9 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6MG4W9ENFJ"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
